a12f9174b628d9319e5366737b58f789b4548344
Susisu <susisu2413@yahoo.co.jp>
2021-01-04 16:02:39 +0900

    fix: fix typo in type name

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import React, { useRef } from "react";
 
-export type Souce<T> = () => T;
+export type Source<T> = () => T;
 
 export type Sink<T> = (val: T) => void;
 
 /**
  * Creates a pair of souce and sink.
  */
-export function useSourceSink<T>(val: T): [Souce<T>, Sink<T>, React.MutableRefObject<T>] {
+export function useSourceSink<T>(val: T): [Source<T>, Sink<T>, React.MutableRefObject<T>] {
   const ref = useRef(val);
   const sourceRef = useRef((): T => ref.current);
   const sinkRef = useRef((val: T): void => { ref.current = val; });
