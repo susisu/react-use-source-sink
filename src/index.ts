a50e1a086684139d13c9d16f088bf2852a87a13f
Susisu <susisu2413@yahoo.co.jp>
2021-01-04 16:05:55 +0900

    style: change import style

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { MutableRefObject, useRef } from "react";
 
 export type Source<T> = () => T;
 
@@ -7,7 +7,7 @@ export type Sink<T> = (val: T) => void;
 /**
  * Creates a pair of souce and sink.
  */
-export function useSourceSink<T>(val: T): [Source<T>, Sink<T>, React.MutableRefObject<T>] {
+export function useSourceSink<T>(val: T): [Source<T>, Sink<T>, MutableRefObject<T>] {
   const ref = useRef(val);
   const sourceRef = useRef((): T => ref.current);
   const sinkRef = useRef((val: T): void => { ref.current = val; });
